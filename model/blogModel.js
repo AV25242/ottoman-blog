53bd9a804455963dc9ed74ec50eb56d34f2f1372
Arun Vijayraghavan <arunv.vijay@gmail.com>
2020-09-10 21:54:51 -0500

    tested validation

--- model/blogModel.js
+++ model/blogModel.js
@@ -1,11 +1,22 @@
 const ottoman = require('ottoman')
-const { model } = require('ottoman');
-const  blogSchema  = require('./schema/blogSchema');
 
+const BlogSchema = new ottoman.Schema({
+  blogKey : String,
+  title:  { type: String, required: true },
+  author: String,  // String is shorthand for {type: String}
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  status: { type: String, enum: ["Close", "Open", "Review"] },
+  meta: {
+    votes: { type: Number, min: 0, max: 5 },
+    favs:  Number
+  }
+});
 
-
-const Blog = ottoman.model('blogs', blogSchema, {scopeName : 'region-us', collectionName: 'blogs'})
+const BlogModel = ottoman.model('blogs', BlogSchema, {scopeName : 'region-us', collectionName: 'blogs'})
 
 module.exports = {
-  Blog
+  BlogModel
 };
